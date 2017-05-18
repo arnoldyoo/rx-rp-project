603fedb7f79106e02e5b7847d91934d62486869f
airnold <jsyoo@bistel.com>
2017-05-18 21:35:19 +0900

    feat(step01) : git user request and response.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { StreamService } from './stream.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'rx-rp-project';
+  gitUserList: Array<any>;
+
+  constructor(
+    private streamS: StreamService
+  ) { }
+
+  ngOnInit() {
+    this.streamS.setStartupStream('https://api.github.com/users');
+    this.streamS.setResponseStream();
+    this.streamS.getResponseStream().subscribe(list => {
+      this.gitUserList = list;
+    });
+
+  }
+
 }
--- src/app/stream.service.ts
+++ src/app/stream.service.ts
@@ -1,8 +1,25 @@
+import { Observable } from 'rxjs/Rx';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class StreamService {
+  startupStream: Observable<any>;
+  responseStream: Observable<any>;
 
   constructor() { }
+
+  setStartupStream(url: string) {
+    this.startupStream = Observable.of(url);
+  }
+
+  setResponseStream() {
+    this.responseStream = this.startupStream.flatMap(url => {
+      return Observable.fromPromise($.getJSON(url));
+    });
+  }
+
+  getResponseStream(): Observable<any> {
+    return this.responseStream;
+  }
 
 }
