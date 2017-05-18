c2f94c2ae7d8043785d7c7b1f464f241b8702130
airnold <jsyoo@bistel.com>
2017-05-18 21:57:27 +0900

    feat(step02) : add refresh button and merge startup stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,30 @@
 import { StreamService } from './stream.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'rx-rp-project';
   gitUserList: Array<any>;
+  @ViewChild('refreshbtn') refreshbtn: ElementRef;
 
   constructor(
-    private streamS: StreamService
+    private streamS: StreamService,
+    private elem: ElementRef
   ) { }
 
   ngOnInit() {
     this.streamS.setStartupStream('https://api.github.com/users');
+    this.streamS.setRequestOnRefreshStream(this.refreshbtn.nativeElement, 'click');
+
     this.streamS.setResponseStream();
+
     this.streamS.getResponseStream().subscribe(list => {
       this.gitUserList = list;
     });
-
   }
 
 }
--- src/app/stream.service.ts
+++ src/app/stream.service.ts
@@ -6,19 +6,40 @@ export class StreamService {
   startupStream: Observable<any>;
   responseStream: Observable<any>;
 
+  refreshClickStream: Observable<any>;
+  requestOnRefreshStream: Observable<any>;
+
   constructor() { }
 
   setStartupStream(url: string) {
     this.startupStream = Observable.of(url);
   }
 
-  setResponseStream() {
-    this.responseStream = this.startupStream.flatMap(url => {
-      return Observable.fromPromise($.getJSON(url));
+  setRequestOnRefreshStream(selector: any, event_kind: string) {
+    this.refreshClickStream = Observable.fromEvent(selector, event_kind);
+
+    this.requestOnRefreshStream = this.refreshClickStream.map(ev => {
+      const randomOffset: number = Math.floor(Math.random() * 500);
+      return 'https://api.github.com/users?since=' + randomOffset;
     });
   }
 
+  //  ----s-----------------> startupstream
+  //  ---------s-----s------> requestOnRefreshSteram
+  //          merge
+  //  ----s----s-----s------> requestStream
+  //      \    \     \
+  //       --r  -r    --r
+  //          flatmap
+  //  --------R-----R----R--> responseSteram
+
+  setResponseStream() {
+    const requestStream: Observable<any> = this.startupStream.merge(this.requestOnRefreshStream);
+    this.responseStream = requestStream.flatMap((url: string) => Observable.fromPromise($.getJSON(url)));
+  }
+
   getResponseStream(): Observable<any> {
+    console.log(this.responseStream);
     return this.responseStream;
   }
 
